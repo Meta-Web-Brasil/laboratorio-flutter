e18518408b938c17558873a8193d14d56fed567a
Ricardo Ramos dos Santos <ramos.ti@gmail.com>
2021-12-07 02:41:06 -0300

    [ESTUDO-FLUTTER]Adicionando stateFull

--- lib/models/signo.dart
+++ lib/models/signo.dart
@@ -1,12 +1,16 @@
+import 'dart:ffi';
+
 class Signo {
   String icone;
   String nome;
   String casa;
   String elemento;
+  double preco;
 
   Signo(
       {required this.icone,
       required this.nome,
       required this.casa,
-      required this.elemento});
+      required this.elemento,
+      required this.preco});
 }
--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,33 +1,72 @@
+import 'package:aula_01/models/signo.dart';
 import 'package:aula_01/repositories/signos_repositories.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 
-class MoedasPage extends StatelessWidget {
+class MoedasPage extends StatefulWidget {
   const MoedasPage({Key? key}) : super(key: key);
 
   @override
+  State<MoedasPage> createState() => _MoedasPageState();
+}
+
+class _MoedasPageState extends State<MoedasPage> {
+  @override
   Widget build(BuildContext context) {
     final listaTodosSignos = SignosRepository.listarTodos;
+    NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+    List<Signo> signosSelecionados = [];
+    double circularRadius = 12;
+    String appBarTitlePage = 'Signos';
+    double fontSizePage = 17;
 
     return Scaffold(
-      appBar: AppBar(title: Text('Hello World!')),
+      appBar: AppBar(title: Text(appBarTitlePage)),
       body: ListView.separated(
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-              // leading: Image.asset(listaTodosSignos[index].icone, ),
-              leading: SvgPicture.asset(
-                listaTodosSignos[index].icone,
-                // color: Colors.red,
-                height: 35,
-                width: 35,
-                semanticsLabel: listaTodosSignos[index].nome,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(circularRadius)),
               ),
-              title: Text(listaTodosSignos[index].nome),
-              trailing: Text(listaTodosSignos[index].elemento),
+              // leading: Image.asset(listaTodosSignos[index].icone, ),
+              leading: (signosSelecionados.contains(listaTodosSignos[index]))
+                  ? CircleAvatar(
+                      child: Icon(Icons.check),
+                    )
+                  : SizedBox(
+                      child: SvgPicture.asset(
+                        listaTodosSignos[index].icone,
+                        // color: Colors.red,
+                        height: 35,
+                        width: 35,
+                        semanticsLabel: listaTodosSignos[index].nome,
+                      ),
+                      width: 40,
+                    ),
+              title: Text(
+                listaTodosSignos[index].nome,
+                style: TextStyle(
+                    fontSize: fontSizePage, fontWeight: FontWeight.w500),
+              ),
+              trailing: Text(
+                listaTodosSignos[index].elemento,
+              ),
+              selected: signosSelecionados.contains(listaTodosSignos[index]),
+              selectedTileColor: Colors.indigo[50],
+              onLongPress: () {
+                setState(() {
+                  (signosSelecionados.contains(listaTodosSignos[index]))
+                      ? signosSelecionados.remove(listaTodosSignos[index])
+                      : signosSelecionados.add(listaTodosSignos[index]);
+                });
+
+                print(listaTodosSignos[index].nome);
+              },
             );
           },
           padding: EdgeInsets.all(16),
-          separatorBuilder: (context, index) => Divider(),
+          separatorBuilder: (_, __) => Divider(),
           itemCount: listaTodosSignos.length),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
